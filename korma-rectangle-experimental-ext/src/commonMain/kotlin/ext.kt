524919acbf3b64eaa97036d0697c3dcfa45e301c
Sergei Bulgakov <servbul@yandex.ru>
2021-01-09 18:14:29 +0300

    Remove properies that should come in KorMA
    This change should come with korlibs/korge-next#109

--- korma-rectangle-experimental-ext/src/commonMain/kotlin/ext.kt
+++ korma-rectangle-experimental-ext/src/commonMain/kotlin/ext.kt
@@ -2,11 +2,6 @@ package com.soywiz.korma.geom
 
 import com.soywiz.korma.geom.*
 
-val IRectangleInt.topLeft get() = PointInt(left, top)
-val IRectangleInt.topRight get() = PointInt(right, top)
-val IRectangleInt.bottomLeft get() = PointInt(left, bottom)
-val IRectangleInt.bottomRight get() = PointInt(right, bottom)
-
 val IRectangleInt.x2: Int get() = x + width - 1
 val IRectangleInt.y2: Int get() = y + height - 1
 
